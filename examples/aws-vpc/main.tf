d04dd475461921e149f49089347e34938121e185
RaduLupan <github@lupan.ca>
2020-07-19 09:38:05 -0400

    Added remote tfstate backend

--- examples/aws-vpc/main.tf
+++ examples/aws-vpc/main.tf
@@ -1,5 +1,16 @@
+terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    bucket         = "terraform-state-dev-us-east-2-fkaymsvstthc"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-locks-dev-us-east-2"
+    encrypt        = true
+  }
+}
 provider "aws" {
-    region = var.region
+    version = "2.70.0"
+    region  = var.region
 }
 
 locals {
