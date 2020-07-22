0b0d74e0987ba672e3866b39c63851d3976bd828
RaduLupan <github@lupan.ca>
2020-07-22 08:27:51 -0400

    Identation

--- examples/aws-vpc/variables.tf
+++ examples/aws-vpc/variables.tf
@@ -1,15 +1,16 @@
 variable "region" {
-    type = string
     description = "AWS Region"
+    type        = string
+    
 }
 
 variable "vpcCidr" {
-    type = string
     description = "VPC CIDR Block"
+    type        = string
     default = "10.0.0.0/16"
 }
 
 variable "environment" {
-    type = string
     description   = "Environment i.e. dev, test, stage, prod" 
+    type          = string
 }
