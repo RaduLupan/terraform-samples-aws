1e1848ae1bfc012092a3a58f7654070201476645
RaduLupan <github@lupan.ca>
2020-07-27 06:59:53 -0400

    Added variables for remote state bucket and remote state key

--- modules/services/webserver-cluster/variables.tf
+++ modules/services/webserver-cluster/variables.tf
@@ -9,10 +9,26 @@ variable "environment" {
     type          = string
 }
 
-variable "vpc_id" {
-    description = "The VPC ID for the VPC where the auto scaling group will be deployed"
+variable "vpc_remote_state_bucket" {
+    description = "The name of the S3 bucket for the VPC's remote state"
     type        = string
 }
+
+variable "vpc_remote_state_key" {
+    description = "The path for the VPC's remote state in S3"
+    type        = string
+}
+
+variable "db_remote_state_bucket" {
+    description = "The name of the S3 bucket for the MySQL DB remote state"
+    type        = string
+}
+
+variable "db_remote_state_key" {
+    description = "The path for the DB's remote state in S3"
+    type        = string
+}
+
 variable "instance_type" {
     description = "EC2 instance type"
     type        = string
