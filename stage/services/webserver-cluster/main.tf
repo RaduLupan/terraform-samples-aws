06e758b5b9b5f285ad5c309c80b9d99ca6232829
RaduLupan <github@lupan.ca>
2020-07-27 07:11:01 -0400

    Added config for webserver-cluster module invocation

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -3,6 +3,26 @@ provider "aws" {
     region  = var.region
 }
 
-module "webserver_cluster" {
-    source "../../../modules/services/webserver-cluster"
+terraform {
+  required_version = ">= 0.12"
+  
+  # Partial configuration. The other arguments i.e. bucket, region, will be passed in from backend.hcl file 
+  # via terraform init -backend-config ../../backend.hcl.
+  # Variables are not allowed in the backend block!
+  backend "s3" {  
+    key            = "stage/services/webserver-cluster/terraform.tfstate"
+  }
+}
+
+module "webserver-cluster" {
+    source = "../../../modules/services/webserver-cluster"
+    
+    region                  = var.region
+    environment             = "stage"
+    vpc_remote_state_bucket = "terraform-state-dev-us-east-2-fkaymsvstthc"
+    vpc_remote_state_key    = "stage/vpc/terraform.tfstate"
+    db_remote_state_bucket  = "terraform-state-dev-us-east-2-fkaymsvstthc"
+    db_remote_state_key     = "stage/data-stores/mysql/terraform.tfstate"
+    instance_type           = "t3.micro"
+    server_port             = 8080
 }
