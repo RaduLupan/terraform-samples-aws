c231acb971cb4b1f89681dc4c11441bf52912148
RaduLupan <github@lupan.ca>
2020-08-04 08:14:31 -0400

    Removed unused variables

--- modules/cluster/asg-rolling-deploy/outputs.tf
+++ modules/cluster/asg-rolling-deploy/outputs.tf
@@ -1,7 +1,3 @@
-output "alb-dns-name" {
-    value = aws_lb.web.dns_name
-}
-
 output "asg-name" {
     value = aws_autoscaling_group.web.name
 }
--- modules/cluster/asg-rolling-deploy/variables.tf
+++ modules/cluster/asg-rolling-deploy/variables.tf
@@ -24,16 +24,6 @@ variable "vpc_remote_state_key" {
     type        = string
 }
 
-variable "db_remote_state_bucket" {
-    description = "The name of the S3 bucket for the MySQL DB remote state"
-    type        = string
-}
-
-variable "db_remote_state_key" {
-    description = "The path for the DB's remote state in S3"
-    type        = string
-}
-
 variable "instance_type" {
     description = "EC2 instance type"
     type        = string
@@ -47,11 +37,6 @@ variable "min_size" {
 
 variable "max_size" {
     description = "The maximum number of EC2 instances in the ASG"
-    type        = number
-}
-
-variable "server_port" {
-    description = "Internal server port for HTTP"
     type        = number
 }
 
@@ -70,10 +55,3 @@ variable "ami" {
     type        = string 
     default     = "ami-0a63f96e85105c6d3"
 }
-
-variable "server_text" {
-    description = "The text the web server should return"
-    type        = string 
-    default     = "Hello, World"
-}
-
