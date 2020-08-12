c1f825ecb003986da6ad2a391ba6e36bcfce3f01
RaduLupan <github@lupan.ca>
2020-08-12 07:41:24 -0400

    Added relevant outputs to asg and alb modules

--- modules/cluster/asg-rolling-deploy/outputs.tf
+++ modules/cluster/asg-rolling-deploy/outputs.tf
@@ -2,3 +2,9 @@ output "asg_name" {
     description = "The name of the Auto Scaling Group"
     value       = aws_autoscaling_group.web.name
 }
+
+output "instance_security_group_id" {
+    description = "The ID of the EC2 instance Security Group"
+    value       = aws_security_group.launch_config.id
+}
+
--- modules/networking/alb/outputs.tf
+++ modules/networking/alb/outputs.tf
@@ -1,3 +1,14 @@
-output "alb-dns-name" {
-    value = aws_lb.web.dns_name
+output "alb_dns_name" {
+    description = "The domain name of the load balancer"
+    value       = aws_lb.web.dns_name
+}
+
+output "alb_http_listener_arn" {
+    description = "The ARN of the HTTP listener"
+    value       = aws_lb_listener.http.arn
+}
+
+output "alb_security_group_id" {
+    description = "The ALB Security Group ID"
+    value       = aws_security_group.alb.id
 }
