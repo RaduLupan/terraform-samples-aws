b23b5872acc93e42c41225156c9b3fac5e8192a6
RaduLupan <github@lupan.ca>
2020-08-12 07:44:18 -0400

    Deleted ebserver-cluster module as it's been broken down in smaller modules

--- modules/services/webserver-cluster/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "alb-dns-name" {
-    value = aws_lb.web.dns_name
-}
-
-output "asg-name" {
-    value = aws_autoscaling_group.web.name
-}
--- modules/services/webserver-cluster/user-data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cat > index.html <<EOF
-<h1>${server_text}</h1>
-<p>DB endpoint: ${db_address}</p>
-<p>DB port: ${db_port}</p>
-EOF
-
-nohup busybox httpd -f -p ${server_port} &
