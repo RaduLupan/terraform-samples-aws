7adf7b38ef2704bf313cf3641596109677777652
RaduLupan <github@lupan.ca>
2020-08-18 08:18:23 -0400

    Go file that for unit testing the alb module-uses Terratest

---
+++ test/alb_example_test.go
@@ -0,0 +1,41 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+	"github.com/gruntwork-io/terratest/modules/http-helper"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestAlbExample(t *testing.T) {
+	opts := &terraform.Options{
+		TerraformDir: "../../../terraform-samples-aws/examples/alb",
+	}
+
+	// Clean up everything at the end of the test
+	defer terraform.Destroy(t, opts)
+
+	// Deploy the example
+	terraform.InitAndApply(t, opts)
+
+	// Get the URL of the ALB
+	albDNSName := terraform.OutputRequired(t, opts, "alb_dns_name")
+	url := fmt.Sprintf("http://%s", albDNSName)
+
+	// Test that the ALB's default action is working and returns a 404
+	expectedStatus := 404
+	expectedBody := "404: page not found"
+
+	maxRetries := 10
+	timeBetweenRetries := 10 * time.Second
+
+	http_helper.HttpGetWithRetry(
+		t,
+		url,
+		expectedStatus,
+		expectedBody,
+		maxRetries,
+		timeBetweenRetries,
+	)
+}
