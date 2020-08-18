b40275137ad232d4128f41ac27c36f6b38f9feb8
RaduLupan <github@lupan.ca>
2020-08-18 08:07:18 -0400

    Deleted region and vpc remote state variables, since subnet_ids are used now

--- modules/networking/alb/variables.tf
+++ modules/networking/alb/variables.tf
@@ -1,8 +1,3 @@
-variable "region" {
-    description = "AWS Region"
-    type        = string
-}
-
 variable "environment" {
     description   = "Environment i.e. dev, test, stage, prod" 
     type          = string
@@ -11,16 +6,6 @@ variable "environment" {
 variable "alb_name" {
     description   = "The name to use for this ALB" 
     type          = string
-}
-
-variable "vpc_remote_state_bucket" {
-    description = "The name of the S3 bucket for the VPC's remote state"
-    type        = string
-}
-
-variable "vpc_remote_state_key" {
-    description = "The path for the VPC's remote state in S3"
-    type        = string
 }
 
 variable "subnet_ids" {
