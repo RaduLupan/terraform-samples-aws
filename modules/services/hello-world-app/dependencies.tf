281d3dff61b5c700fbda1a94a8841160be00e4a8
RaduLupan <github@lupan.ca>
2020-08-20 08:23:40 -0400

    Introduced conditional count for all dependencies to allow for resource mocking (simulating).

--- modules/services/hello-world-app/dependencies.tf
+++ modules/services/hello-world-app/dependencies.tf
@@ -1,5 +1,8 @@
 # Use this data source to get the VPC Id output from the remote state file of the vpc module.
 data "terraform_remote_state" "vpc" {
+    # If var.vpc_id has a non-null value that means that an existing VPC is being used so no need to construct the data source. 
+    count var.vpc_id == null ? 1 : 0
+
     backend = "s3"
 
     config = {
@@ -11,6 +14,9 @@ data "terraform_remote_state" "vpc" {
 
 # Use this data source to get the db-endpoint and port outputs from the remote state file of the mysql module.
 data "terraform_remote_state" "db" {
+    # If var.mysql_config has a non-null value that means that the database is being mocked (simulated) so no need to construct the data source. 
+    count = var.mysql_config == null ? 1 : 0
+
     backend = "s3"
 
     config = {
@@ -22,6 +28,9 @@ data "terraform_remote_state" "db" {
 
 # Use this data source to provide the set of subnet IDs in our VPC that are tagged tier=private
 data "aws_subnet_ids" "private_subnet" {
+  # If var.private_subnet_ids has a non-null value that means that existing subnets are being used so no need to construct the data source.
+  count = var.private_subnet_ids == null ? 1 : 0
+
   vpc_id = local.vpc_id
 
   tags = {
@@ -31,6 +40,9 @@ data "aws_subnet_ids" "private_subnet" {
 
 # Use this data source to provide the set of subnet IDs in our VPC that are tagged tier=public.
 data "aws_subnet_ids" "public_subnet" {
+  # If var.public_subnet_ids has a non-null value that means that existing subnets are being used so no need to construct the data source.
+  count var.public_subnet_ids == null ? 1 : 0
+  
   vpc_id = local.vpc_id
 
   tags = {
