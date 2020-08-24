39af888402a860ca41c5ee9eeac8a2974d3b0233
RaduLupan <github@lupan.ca>
2020-08-24 08:17:28 -0400

    Initial commit

---
+++ test/asg_example_test.go
@@ -0,0 +1,19 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestAlbExample(t *testing.T) {
+	opts := &terraform.Options{
+		TerraformDir: "../../../terraform-samples-aws/examples/asg",
+	}
+
+	// Clean up everything at the end of the test. Defer ensures that this runs regardless of the exit code of the function.
+	defer terraform.Destroy(t, opts)
+
+	// Deploy the example
+	terraform.InitAndApply(t, opts)
+}
