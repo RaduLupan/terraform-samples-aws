fcded94eacc67273eba3e73134c3af30cf52224c
RaduLupan <github@lupan.ca>
2020-08-24 08:14:06 -0400

    Fixed syntax errors

--- modules/services/hello-world-app/dependencies.tf
+++ modules/services/hello-world-app/dependencies.tf
@@ -1,7 +1,7 @@
 # Use this data source to get the VPC Id output from the remote state file of the vpc module.
 data "terraform_remote_state" "vpc" {
     # If var.vpc_id has a non-null value that means that an existing VPC is being used so no need to construct the data source. 
-    count var.vpc_id == null ? 1 : 0
+    count = var.vpc_id == null ? 1 : 0
 
     backend = "s3"
 
@@ -41,7 +41,7 @@ data "aws_subnet_ids" "private_subnet" {
 # Use this data source to provide the set of subnet IDs in our VPC that are tagged tier=public.
 data "aws_subnet_ids" "public_subnet" {
   # If var.public_subnet_ids has a non-null value that means that existing subnets are being used so no need to construct the data source.
-  count var.public_subnet_ids == null ? 1 : 0
+  count = var.public_subnet_ids == null ? 1 : 0
   
   vpc_id = local.vpc_id
 
