36cf9d29460d3ed453cba105bc6a206837ab1a54
RaduLupan <github@lupan.ca>
2020-08-24 08:07:23 -0400

    Added variables to hello-world-app example

---
+++ examples/hello-world-app/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+    description = "AWS Region"
+    type        = string
+    default     = "us-east-2"
+}
+
+variable "mysql_config" {
+    description = "The config for the MySQL DB"
+
+    type        = object({
+        address = string
+        port    = number
+    })
+
+    default     = {
+        address = "simulated-mysql-db-endpoint"
+        port    = 12345
+    }
+}
