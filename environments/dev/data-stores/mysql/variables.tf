a69edd3cc85aaa110543cb33d1f85f6a6821de6a
RaduLupan <github@lupan.ca>
2020-08-28 08:19:17 -0400

    Added db_password and db_name variables to dev environment-mysql

--- environments/dev/data-stores/mysql/variables.tf
+++ environments/dev/data-stores/mysql/variables.tf
@@ -3,3 +3,14 @@ variable "region" {
     type        = string
 
 }
+
+variable "db_password" {
+    description = "The admin password for the database"
+    type        = string
+}
+
+variable "db_name" {
+    description = "The name to use for the database"
+    type        = string
+    default     = "example_database_dev"
+}
