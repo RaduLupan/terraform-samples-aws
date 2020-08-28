f9a2219280174225922afe205c5a434f0a8217c8
RaduLupan <github@lupan.ca>
2020-08-28 08:23:10 -0400

    Replaced hardcoded values with variables

--- examples/mysql/main.tf
+++ examples/mysql/main.tf
@@ -12,7 +12,8 @@ module "mysql" {
     
     region                  = var.region
     environment             = var.environment
-    db_name                 = "exampledb"
-    db_password             = "DontLeaveMeInPlainText!"
+    db_name                 = var.db_name
+    db_username             = var.db_username
+    db_password             = var.db_password
     subnet_ids              = data.aws_subnet_ids.default.ids
 }
