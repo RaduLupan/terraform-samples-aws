5f05c611aa1032863437a4c588ab6092cafcfefc
RaduLupan <github@lupan.ca>
2020-08-28 09:21:11 -0400

    Reorganized variables in required and optional

--- environments/dev/data-stores/mysql/variables.tf
+++ environments/dev/data-stores/mysql/variables.tf
@@ -1,16 +1,37 @@
-variable "region" {
-    description = "AWS Region"
-    type        = string
-
-}
+#----------------------------------------------------------------------------
+# REQUIRED PARAMETERS: You must provide a value for each of these parameters.
+#----------------------------------------------------------------------------
 
 variable "db_password" {
     description = "The admin password for the database"
     type        = string
 }
 
+
+#---------------------------------------------------------------
+# OPTIONAL PARAMETERS: These parameters have resonable defaults.
+#---------------------------------------------------------------
+
+variable "region" {
+    description = "AWS Region"
+    type        = string
+    default     = "us-east-2"
+}
+
+variable "environment" {
+    description   = "Environment i.e. dev, test, stage, prod" 
+    type          = string
+    default       = "dev"
+}
+
 variable "db_name" {
     description = "The name to use for the database"
     type        = string
-    default     = "example_database_dev"
+    default     = "example_database"
+}
+
+variable "db_username" {
+    description = "The admin username for the database"
+    type        = string
+    default     = "admin"
 }
