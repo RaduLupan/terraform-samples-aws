92861166515682eb1403d9f9137a8b4dd23bf9d5
RaduLupan <github@lupan.ca>
2020-08-28 09:21:49 -0400

    Replaced hardcoded values with variables

--- environments/dev/data-stores/mysql/main.tf
+++ environments/dev/data-stores/mysql/main.tf
@@ -16,11 +16,12 @@ module "mysql" {
   source = "../../../../modules/data-stores/mysql"
   
   region                  = var.region
-  environment             = "stage"
+  environment             = var.environment
+  
   vpc_remote_state_bucket = "terraform-state-dev-us-east-2-fkaymsvstthc"
   vpc_remote_state_key    = "environments/dev/vpc/terraform.tfstate"
-  instance_class          = "db.t2.micro"
-  allocated_storage_gb    = 10
-  db_name                 = "webdb"
-  db_password             = "DontLeaveMeInClearText!" 
+  
+  db_name                 = var.db_name
+  db_username             = var.db_username 
+  db_password             = var.db_password
 }
