b786915df4887b7f95fec24e5cbbecb8d40bdda1
RaduLupan <github@lupan.ca>
2020-08-31 07:05:14 -0400

    Organized variables in required and optional

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,16 +1,24 @@
+#----------------------------------------------------------------------------
+# REQUIRED PARAMETERS: You must provide a value for each of these parameters.
+#----------------------------------------------------------------------------
+
 variable "region" {
     description = "AWS Region"
     type        = string
     
 }
 
+variable "environment" {
+    description   = "Environment i.e. dev, test, stage, prod" 
+    type          = string
+}
+
+#---------------------------------------------------------------
+# OPTIONAL PARAMETERS: These parameters have resonable defaults.
+#---------------------------------------------------------------
+
 variable "vpcCidr" {
     description = "VPC CIDR Block"
     type        = string
     default = "10.0.0.0/16"
-}
-
-variable "environment" {
-    description   = "Environment i.e. dev, test, stage, prod" 
-    type          = string
 }
