1421f24080ee49dca5bd5bed1e8d4894ed223e05
RaduLupan <github@lupan.ca>
2020-09-01 07:37:55 -0400

    Introduced aws module and GetRandomStableRegion function to test the VPC in all regions

--- test/vpc_example_test.go
+++ test/vpc_example_test.go
@@ -3,18 +3,22 @@ package test
 import (
 	"testing"
 
+	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
 func TestVPCExample(t *testing.T) {
 	t.Parallel()
 
+	// https://github.com/gruntwork-io/terratest/blob/master/test/terraform_aws_network_example_test.go
+	// Pick a random AWS region to test in. This helps ensure your code works in all regions.
+	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
+
 	opts := &terraform.Options{
 		TerraformDir: "../../../terraform-samples-aws/examples/vpc",
 
 		Vars: map[string]interface{}{
-			// RDS DB name won't allow "-" use "_" instead as a separator.
-			"region":      "us-west-2",
+			"region":      awsRegion,
 			"vpcCidr":     "192.168.0.0/16",
 			"environment": "test",
 		},
