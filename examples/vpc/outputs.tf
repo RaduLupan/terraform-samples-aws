01269d37a7970f44a61887905cc2b4b8aa77e49d
RaduLupan <github@lupan.ca>
2020-09-02 08:07:57 -0400

    Added outputs for first public and private subnet id to be tested by Terratest aws.IsPublicSubnet function

--- examples/vpc/outputs.tf
+++ examples/vpc/outputs.tf
@@ -3,3 +3,17 @@ output "vpc_id" {
     description = "The ID of the VPC"
     value       = module.vpc.vpc_id
 }
+
+# The first public subnet is used in the Terratest test file to verify that 
+# the public subnet is indeed public (there is a rule in its route table with an "igw-" target).
+output "public_subnet_id" {
+    description = "The ID of the first public subnet"
+    value       = module.vpc.public_subnet_id
+}
+
+# The first private subnet is used in the Terratest test file to verify that 
+# the private subnet is indeed private (there is NO rule in its route table with an "igw-" target).
+output "private_subnet_id" {
+    description = "The ID of the first private subnet"
+    value       = module.vpc.private_subnet_id
+}
--- modules/networking/vpc/outputs.tf
+++ modules/networking/vpc/outputs.tf
@@ -2,3 +2,17 @@ output "vpc_id" {
     description = "The ID of the VPC"
     value       = aws_vpc.main.id
 }
+
+# The first public subnet is used in the Terratest test file to verify that 
+# the public subnet is indeed public (there is a rule in its route table with an "igw-" target).
+output "public_subnet_id" {
+    description = "The ID of the first public subnet"
+    value       = aws_subnet.public_subnet[0].id
+}
+
+# The first private subnet is used in the Terratest test file to verify that 
+# the private subnet is indeed private (there is NO rule in its route table with an "igw-" target).
+output "private_subnet_id" {
+    description = "The ID of the first private subnet"
+    value       = aws_subnet.private_subnet[0].id
+}
