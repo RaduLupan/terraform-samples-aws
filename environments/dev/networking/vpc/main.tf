baa61acbe09fa0f3ca5348fc08311f2652afb6b6
RaduLupan <github@lupan.ca>
2020-09-03 06:47:14 -0400

    Moved the vpc instance on dev environment to the networking folder where it belongs

--- environments/dev/networking/vpc/main.tf
+++ environments/dev/networking/vpc/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 module "vpc" {
-    source = "../../../modules/networking/vpc"
+    source = "../../../../modules/networking/vpc"
    
     region      = var.region
     vpcCidr     = var.vpcCidr
