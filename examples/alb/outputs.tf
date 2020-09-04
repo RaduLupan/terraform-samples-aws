d7892be8e1bc6ee627c0cc92ed66b0be433fda80
RaduLupan <github@lupan.ca>
2020-09-04 07:26:26 -0400

    Added all outputs provided by the alb module

--- examples/alb/outputs.tf
+++ examples/alb/outputs.tf
@@ -2,3 +2,13 @@ output "alb_dns_name" {
     description = "The domain of the load balancer"
     value       = module.alb.alb_dns_name
 }
+
+output "alb_http_listener_arn" {
+    description = "The ARN of the HTTP listener"
+    value       = module.alb.alb_http_listener_arn
+}
+
+output "alb_security_group_id" {
+    description = "The ALB Security Group ID"
+    value       = module.alb.alb_security_group_id
+}
