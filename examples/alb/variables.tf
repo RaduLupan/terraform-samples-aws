a6775752dae4ab884f569bfcf03480ad73eae5fb
RaduLupan <github@lupan.ca>
2020-09-04 06:44:17 -0400

    Introduced variable for subnet_ids to condition the dependencies on whether or not it is null

--- examples/alb/variables.tf
+++ examples/alb/variables.tf
@@ -1,5 +1,16 @@
+#---------------------------------------------------------------
+# OPTIONAL PARAMETERS: These parameters have resonable defaults.
+#---------------------------------------------------------------
+
 variable "alb_name" {
     description = "The name of the ALB and all its resources"
     type        = string
     default     = "terraform-samples"
 }
+
+variable "subnet_ids" {
+    description = "The IDs for subnets that the ALB sits on"
+    type        = list(string)
+    default     = null  
+}
+
