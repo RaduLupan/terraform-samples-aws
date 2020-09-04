9ddc9ba6b915411993801f71aae3a99d973b424f
RaduLupan <github@lupan.ca>
2020-09-04 08:08:48 -0400

    Introduced subnet_ids variable to allow for conditional dependencies based on whether or not subnet_ids=null

--- examples/asg/variables.tf
+++ examples/asg/variables.tf
@@ -1,3 +1,7 @@
+#---------------------------------------------------------------
+# OPTIONAL PARAMETERS: These parameters have resonable defaults.
+#---------------------------------------------------------------
+
 variable "region" {
     description = "AWS Region"
     type        = string
@@ -7,4 +11,11 @@ variable "region" {
 variable "cluster_name" {
     description   = "The name to use for all the cluster resources" 
     type          = string
+    default       = "example-web"
+}
+
+variable "subnet_ids" {
+    description = "The IDs for subnets that the EC2 instances in the ASG sit on"
+    type        = list(string)
+    default     = null  
 }
