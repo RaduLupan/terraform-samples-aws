cae76de3246377a73ac053ae5f5a3d83602644c5
RaduLupan <github@lupan.ca>
2020-09-05 09:32:02 -0400

    Added default value to region variable and deleted terraform.tfvars

--- environments/dev/services/hello-world-app/variables.tf
+++ environments/dev/services/hello-world-app/variables.tf
@@ -1,5 +1,10 @@
+#---------------------------------------------------------------
+# OPTIONAL PARAMETERS: These parameters have resonable defaults.
+#---------------------------------------------------------------
+
 variable "region" {
     description = "AWS Region"
     type        = string
+    default     = "us-east-2"
 
 }
