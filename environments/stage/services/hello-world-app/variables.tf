efafe145e4edb997c780753c649424fe26f1f1cf
RaduLupan <github@lupan.ca>
2020-09-08 07:58:46 -0400

    Added new tier hello-world-app with stage-specific parameter values

---
+++ environments/stage/services/hello-world-app/variables.tf
@@ -0,0 +1,10 @@
+#---------------------------------------------------------------
+# OPTIONAL PARAMETERS: These parameters have resonable defaults.
+#---------------------------------------------------------------
+
+variable "region" {
+    description = "AWS Region"
+    type        = string
+    default     = "us-east-2"
+
+}
