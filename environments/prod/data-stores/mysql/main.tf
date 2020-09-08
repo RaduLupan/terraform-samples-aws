121d351047a714d7d1fb4d102a542551370664b1
RaduLupan <github@lupan.ca>
2020-09-08 08:48:41 -0400

    Modified prod values, replaced hard coded password with db_password variable.

--- environments/prod/data-stores/mysql/main.tf
+++ environments/prod/data-stores/mysql/main.tf
@@ -13,14 +13,19 @@ provider "aws" {
 }
 
 module "mysql" {
-  source = "../../../../modules/data-stores/mysql"
-  
+  # Github source - public repository. Note that the double-slash in the Git URL after the repository name is required.
+  # Also, the v0.0.3 tag had to be pushed using:
+  # git tag -a "v0.0.3" -m "First release"
+  # git push --follow-tags
+
+  source = "github.com/RaduLupan/terraform-samples-aws//modules/data-stores/mysql?ref=v0.0.3"
+    
   region                  = var.region
   environment             = "prod"
   vpc_remote_state_bucket = "terraform-state-dev-us-east-2-fkaymsvstthc"
   vpc_remote_state_key    = "environments/prod/vpc/terraform.tfstate"
-  instance_class          = "db.t2.micro"
-  allocated_storage_gb    = 10
-  db_name                 = "webdb"
-  db_password             = "DontLeaveMeInClearText!" 
+  instance_class          = "db.t3.large"
+  allocated_storage_gb    = 200
+  db_name                 = "webdbadmin"
+  db_password             = var.db_password
 }
