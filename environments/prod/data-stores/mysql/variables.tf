6e0fad36540a8f3f0b831367b75642de914dc416
RaduLupan <github@lupan.ca>
2020-09-08 08:47:00 -0400

    Organized variables in required and optional, deleted terraform.tfvars file

--- environments/prod/data-stores/mysql/variables.tf
+++ environments/prod/data-stores/mysql/variables.tf
@@ -1,5 +1,18 @@
+#----------------------------------------------------------------------------
+# REQUIRED PARAMETERS: You must provide a value for each of these parameters.
+#----------------------------------------------------------------------------
+
+variable "db_password" {
+    description = "The admin password for the database"
+    type        = string
+}
+
+#---------------------------------------------------------------
+# OPTIONAL PARAMETERS: These parameters have resonable defaults.
+#---------------------------------------------------------------
 variable "region" {
     description = "AWS Region"
     type        = string
-
+    default     = "us-east-2"
+    
 }
