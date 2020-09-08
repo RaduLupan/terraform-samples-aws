03e151cc0eadc8a2fbc86f943996fc16935f9d06
RaduLupan <github@lupan.ca>
2020-09-08 09:06:54 -0400

    Sourced the webserver-cluster tier in prod to github tag v0.0.3 and adjusted prod values

--- environments/prod/services/webserver-cluster/main.tf
+++ environments/prod/services/webserver-cluster/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12, < 0.13"
   
   # Partial configuration. The other arguments i.e. bucket, region, will be passed in from backend.hcl file 
-  # via terraform init -backend-config ../../../backend.hcl.
+  # via terraform init -backend-config ../../../../backend.hcl.
   # Variables are not allowed in the backend block!
   backend "s3" {  
     key            = "environments/stage/services/webserver-cluster/terraform.tfstate"
@@ -15,25 +15,24 @@ provider "aws" {
 }
 
 module "webserver-cluster" {
-    # Local source
-    source = "../../../../modules/services/webserver-cluster"
-    
+        
     # Github source - public repository. Note that the double-slash in the Git URL after the repository name is required.
-    # Also, the v0.0.1 tag had to be pushed using:
-    # git tag -a "v0.0.1" -m "First release"
+    # Also, the v0.0.3 tag had to be pushed using:
+    # git tag -a "v0.0.3" -m "First release"
     # git push --follow-tags
-    # source = "github.com/RaduLupan/terraform-samples-aws//modules/services/webserver-cluster?ref=v0.0.3"
+    source = "github.com/RaduLupan/terraform-samples-aws//modules/services/webserver-cluster?ref=v0.0.3"
   
     region                  = var.region
-    environment             = "stage"
-    ami                     = "ami-0a63f96e85105c6d3"
-    server_text             = "Hello from Zero Downtime Deployment"
-    cluster_name            = "terraform-web"
+    environment             = "prod"
+        
     vpc_remote_state_bucket = "terraform-state-dev-us-east-2-fkaymsvstthc"
-    vpc_remote_state_key    = "environments/stage/vpc/terraform.tfstate"
+    vpc_remote_state_key    = "environments/prod/vpc/terraform.tfstate"
+    
     db_remote_state_bucket  = "terraform-state-dev-us-east-2-fkaymsvstthc"
-    db_remote_state_key     = "environments/stage/data-stores/mysql/terraform.tfstate"
-    instance_type           = "t3.micro"
+    db_remote_state_key     = "environments/prod/data-stores/mysql/terraform.tfstate"
+    
+    cluster_name            = "terraform-web"
+    instance_type           = "t3.large"
     min_size                = 2
     max_size                = 10
     server_port             = 8080
