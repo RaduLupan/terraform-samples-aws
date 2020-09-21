c3c635390b503fbc9434178b86d1e715b7b13130
RaduLupan <github@lupan.ca>
2020-09-21 07:01:19 -0400

    Reorganized variables

--- environments/dev/services/hello-world-app/variables.tf
+++ environments/dev/services/hello-world-app/variables.tf
@@ -1,3 +1,15 @@
+variable "db_remote_state_bucket" {
+    description = "The name of the S3 bucket for the MySQL DB remote state"
+    type        = string
+    default     = null
+}
+
+variable "db_remote_state_key" {
+    description = "The path for the DB's remote state in S3"
+    type        = string
+    default     = null
+}
+
 #---------------------------------------------------------------
 # OPTIONAL PARAMETERS: These parameters have resonable defaults.
 #---------------------------------------------------------------
@@ -7,4 +19,16 @@ variable "region" {
     type        = string
     default     = "us-east-2"
 
+}
+
+variable "environment" {
+    description   = "Environment i.e. dev, test, stage, prod" 
+    type          = string
+    default       = "dev"
+}
+
+variable "server_text" {
+    description = "The text the web server should return"
+    type        = string 
+    default     = "Hello, World"
 }
