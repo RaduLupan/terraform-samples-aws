f26822e56fde26fac74f9f31d711eb957c16d2fb
RaduLupan <github@lupan.ca>
2020-09-24 07:57:10 -0400

    Deleted duplicate example

--- examples/standalone/aws-rds-mysql/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "db-endpoint" {
-    description = "Database endpoint"
-    value       = aws_db_instance.db1.address
-}
-
-output "port" {
-    description = "The port the database is listening on"
-    value = aws_db_instance.db1.port
-}
--- examples/standalone/aws-rds-mysql/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-region = "us-east-2"
-environment = "stage"
-vpc_id = "vpc-02814d9f8bdd988a5"
